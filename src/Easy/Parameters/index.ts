2f042b1f67afe00ba8425cbd9a99b7e53f29a144
hisa <hisayuki.k8@gmail.com>
2024-03-14 23:29:36 +0900

    add: parameters type

---
+++ src/Easy/Parameters/index.ts
@@ -0,0 +1,9 @@
+const foo = (arg1: string, arg2: number): void => {}
+
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...any: infer S
+) => any
+  ? S
+  : any
+
+type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]
